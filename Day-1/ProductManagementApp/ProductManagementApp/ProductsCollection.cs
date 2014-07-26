12711c4a15adf8363929889db6638bbde012480c
tkmagesh <tkmagesh77@gmail.com>
2014-07-26 13:30:32 +0530

    Day - 1 - Design Principles

---
+++ Day-1/ProductManagementApp/ProductManagementApp/ProductsCollection.cs
@@ -0,0 +1,148 @@
+﻿using System.Collections;
+
+namespace ProductManagementApp
+{
+    public class ProductsCollection : IEnumerable, IEnumerator
+    {
+        private ArrayList _list = new ArrayList();
+        public void Add(Product product)
+        {
+            _list.Add(product);
+        }
+        public int Count
+        {
+            get { return _list.Count; }
+        }
+
+        public Product this[int index]
+        {
+            get { return (Product) _list[index]; }
+        }
+
+        public Product Get(int index)
+        {
+            return (Product) _list[index];
+
+        }
+        /*IEnumeable's Members*/
+        public IEnumerator GetEnumerator()
+        {
+            return this;
+        }
+
+
+        /*IEnumerator's Members*/
+        private int index = -1;
+        public bool MoveNext()
+        {
+            ++index;
+            if (index >= _list.Count)
+            {
+                Reset();
+                return false;
+            }
+            return true;
+        }
+
+        public void Reset()
+        {
+            index = -1;
+        }
+
+        public object Current
+        {
+            get { return _list[index]; }
+        }
+
+        public void Sort(ICompareProduct comparer)
+        {
+            for(var i=0;i<_list.Count-1;i++)
+                for (var j = i + 1; j < _list.Count; j++)
+                {
+                    var leftProduct = (Product) _list[i];
+                    var rightProduct = (Product)_list[j];
+                    var shouldSwap = comparer.ShouldSwap(leftProduct,rightProduct);
+                    
+                    if (shouldSwap)
+                    {
+                        var temp = leftProduct;
+                        _list[i] =_list[j];
+                        _list[j] = temp;
+                    }
+                }
+        }
+        public ProductsCollection Filter(IProductCriteria criteria)
+        {
+            var result = new ProductsCollection();
+            foreach(var item in _list)
+            {
+                var product = (Product) item;
+                if (criteria.IsSatisfied(product))
+                    result.Add(product);
+            }
+            return result;
+        }
+    }
+
+    public interface IProductCriteria
+    {
+        bool IsSatisfied(Product product);
+    }
+
+    public class CostlyProductCriteria : IProductCriteria
+    {
+        private readonly decimal _basePrice;
+
+        public CostlyProductCriteria(decimal basePrice)
+        {
+            _basePrice = basePrice;
+        }
+
+        public bool IsSatisfied(Product product)
+        {
+            return product.Cost > _basePrice;
+        }
+    }
+
+    public class CategoryCriteria : IProductCriteria
+    {
+        private readonly int _category;
+
+        public CategoryCriteria(int category)
+        {
+            _category = category;
+        }
+
+        public bool IsSatisfied(Product product)
+        {
+            return product.Category == _category;
+        }
+    }
+
+
+
+    public interface ICompareProduct
+    {
+        bool ShouldSwap(Product leftProduct, Product rightProduct);
+    }
+
+    public class CompareProductById : ICompareProduct
+    {
+        public bool ShouldSwap(Product leftProduct, Product rightProduct)
+        {
+            return leftProduct.Id > rightProduct.Id;
+        }
+    }
+
+    public class CompareProductByCost : ICompareProduct
+    {
+        public bool ShouldSwap(Product leftProduct, Product rightProduct)
+        {
+            return leftProduct.Cost > rightProduct.Cost;
+        }
+    }
+
+    
+
+
+}
---
+++ Day-1/ProductManagementApp/ProductManagementApp/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProductManagementApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var products = new ProductsCollection();
+            products.Add(new Product{Id = 6, Name = "Pen", Cost = 10, Units = 10, Category = 1});
+            products.Add(new Product{Id = 4, Name = "Hen", Cost = 90, Units = 30, Category = 2});
+            products.Add(new Product{Id = 9, Name = "Ten", Cost = 30, Units = 20, Category = 1});
+            products.Add(new Product{Id = 2, Name = "Den", Cost = 70, Units = 50, Category = 2});
+            products.Add(new Product{Id = 7, Name = "Zen", Cost = 40, Units = 30, Category = 1});
+            products.Add(new Product{Id = 1, Name = "Len", Cost = 50, Units = 10, Category = 2});
+
+            Console.WriteLine("Initial List....");
+            Console.WriteLine("========================================================");
+            foreach(var product in products)
+                Console.WriteLine(product);
+            Console.WriteLine("========================================================");
+            Console.WriteLine();
+
+            Console.WriteLine("After Sort....");
+            Console.WriteLine("========================================================");
+            products.Sort(new CompareProductById());
+            foreach (var product in products)
+                Console.WriteLine(product);
+            Console.WriteLine("========================================================");
+            Console.WriteLine();
+
+            Console.WriteLine("After Sort by cost....");
+            Console.WriteLine("========================================================");
+            products.Sort(new CompareProductByCost());
+            foreach (var product in products)
+                Console.WriteLine(product);
+            Console.WriteLine("========================================================");
+            Console.WriteLine();
+
+            Console.WriteLine("After Sort by units....");
+            Console.WriteLine("========================================================");
+            products.Sort(new CompareProductByUnits());
+            foreach (var product in products)
+                Console.WriteLine(product);
+            Console.WriteLine("========================================================");
+            Console.WriteLine();
+
+            Console.WriteLine("All costly products (cost > 50)....");
+            Console.WriteLine("========================================================");
+            var costlyProducts = products.Filter(new CostlyProductCriteria(50));
+            foreach (var product in costlyProducts)
+                Console.WriteLine(product);
+            Console.WriteLine("========================================================");
+            Console.WriteLine();
+
+            Console.WriteLine("All products belonging to category 1");
+            Console.WriteLine("========================================================");
+            var cat1Products = products.Filter(new CategoryCriteria(1));
+            foreach (var product in cat1Products)
+                Console.WriteLine(product);
+            Console.WriteLine("========================================================");
+            Console.WriteLine();
+
+            Console.ReadLine();
+        }
+    }
+
+    public class CompareProductByUnits : ICompareProduct
+    {
+        public bool ShouldSwap(Product leftProduct, Product rightProduct)
+        {
+            return leftProduct.Units > rightProduct.Units;
+        }
+    }
+
+}
